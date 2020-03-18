eb39b747d820b392367d20a6f349de19b93627ee
Woonjang Ahn <woonjangahn@gmail.com>
2020-03-18 18:42:38 +0900

    etc : 사용할 더미 데이터들 추가

---
+++ assets/javascript/data/friends.js
@@ -0,0 +1,32 @@
+export const friendsData = [
+  {
+    src: "./assets/image/friends/html_90s.jpg",
+    name: "html_90s",
+    actual: "http://info.cern.ch/"
+  },
+  {
+    src: "./assets/image/friends/css_is_awesome.jpg",
+    name: "css_is_awesome",
+    actual: "https://css-tricks.com/css-is-awesome/"
+  },
+  {
+    src: "./assets/image/friends/hodol.jpg",
+    name: "dev.hodol",
+    actual: "https://www.instagram.com/woonjangahn/"
+  },
+  {
+    src: "./assets/image/friends/java.jpg",
+    name: "__java",
+    actual: "https://en.wikipedia.org/wiki/Coffee_production_in_Indonesia#Java"
+  },
+  {
+    src: "./assets/image/friends/pobi_kotlin.jpg",
+    name: "pobi_kotlin",
+    actual: "https://www.facebook.com/javajigi"
+  },
+  {
+    src: "./assets/image/friends/eastjun.jpg",
+    name: "eastjun",
+    actual: "https://www.facebook.com/junnie0515"
+  },
+]
---
+++ assets/javascript/data/posts.js
@@ -0,0 +1,38 @@
+export const postData = [
+  {
+    index: 2,
+    authorSrc: "./assets/image/friends/html_90s.jpg",
+    authorName: "html_90s",
+    authorLocation: "스위스",
+    likeThumbnail: "./assets/image/friends/pobi_kotlin.jpg",
+    likeName: "pobi_kotlin",
+    likeCount: "523",
+    postSrc: "assets/image/posts/199012.jpg",
+    postContent: "#웹서버 #info.cern.ch",
+    createdAt: "1990년 12월",
+  },
+  {
+    index: 1,
+    authorSrc: "./assets/image/friends/html_90s.jpg",
+    authorName: "html_90s",
+    authorLocation: "연구실 한 켠 어딘가",
+    likeThumbnail: "./assets/image/friends/eastjun.jpg",
+    likeName: "eastjun",
+    likeCount: "789",
+    postSrc: "assets/image/posts/199011.jpg",
+    postContent: "#www #논문읽기",
+    createdAt: "1990년 11월",
+  },
+  {
+    index: 0,
+    authorSrc: "./assets/image/friends/html_90s.jpg",
+    authorName: "html_90s",
+    authorLocation: "유럽 입자 물리 연구소 CERN",
+    likeThumbnail: "./assets/image/friends/hodol.jpg",
+    likeName: "dev.hodol",
+    likeCount: "532",
+    postSrc: "assets/image/posts/199010.jpg",
+    postContent: "#에디터 #hypermedia",
+    createdAt: "1990년 10월",
+  }
+];
