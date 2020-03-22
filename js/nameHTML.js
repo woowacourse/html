ed23388d909aa826a7301511eb582ab63b2a605d
icyMojito <dev.icymojito@gmail.com>
2020-03-23 01:35:37 +0900

    feat : 본문 내용과 디자인 구현

---
+++ js/nameHTML.js
@@ -0,0 +1,45 @@
+const nameHTMLString = '<p id="chars">\n' +
+    '    <span>H</span>yper\n' +
+    '    <span>T</span>ext\n' +
+    '    <span>M</span>arkup\n' +
+    '    <span>L</span>anguage\n' +
+    '</p>\n' +
+    '<span class="subTitle">\n' +
+    '    Hyper Text란?\n' +
+    '</span>\n' +
+    '<p class="description">\n' +
+    '    참조(하이퍼링크)를 통해 독자가 한 문서에서 다른 문서로 즉시 접근할 수 있는 텍스트이다. 주로 컴퓨터나 다른 전자기기들을 통해 표시된다. \'hyper(건너 편의, 초월, 과도한)\'와 \'text\'를 합성하여 만든 컴퓨터 및 인터넷 관련 용어로서, 파생텍스트라고도 한다.\n' +
+    '    기존의 문서가 순차적이면서 서열형 구조라면, 하이퍼텍스트는 링크에 따라 그 차례가 바뀌는 임의적이면서 나열형인 구조를 가진다. 즉, 출판된 책처럼 작가의 의도대로 사용자가 따라가는 것이 아닌, 하이퍼링크로 연결된 문서들을 어떠한 행위(클릭)에 따라 자유롭게 이동할 수 있다.\n' +
+    '    이런 하이퍼텍스트의 등장은 검색엔진과 더불어 정보습득의 새로운 장을 인류에게 가져다 주었다.\n' +
+    '</p>\n' +
+    '<span class="subTitle">\n' +
+    '    Markup Language란?\n' +
+    '</span>\n' +
+    '<p class="description">\n' +
+    '    태그 등을 이용하여 문서나 데이터의 구조를 명기하는 언어의 한 가지이다.\n' +
+    '    태그는 원래 텍스트와는 별도로 원고의 교정부호와 주석을 표현하기 위한 것이였으나 용도가 점차 확장되어 문서의 구조를 표현하는 역할을 하게 되었다. 이러한 태그 방법의 체계를 마크업 언어라 한다.\n' +
+    '</p>\n' +
+    '<span id="smallTitle">\n' +
+    '    Markup Language를 써서 표현하는 이유\n' +
+    '</span>\n' +
+    '<ul class="description" style="margin-left: 60px">\n' +
+    '    <li>웹 브라우저에게 명령하기 위해</li>\n' +
+    '    <li>문서 작성에 대한 협업과 유지 보수를 위해</li>\n' +
+    '    <li>정보를 수집하는 검색 엔진에게 정확한 자료를 제공하기 위해</li>\n' +
+    '</ul>\n' +
+    '<span class="subTitle">\n' +
+    '    그래서 HTML이란\n' +
+    '</span>\n' +
+    '<p class="description" style="text-decoration: underline">\n' +
+    '    웹브라우저에 표시되는 웹페이지를 만들어 달라고 컴퓨터에게 요청하는 언어이자,\n' +
+    '    본문을 위한 구조적 의미를 나타내고 링크, 인용과 그 밖의 항목으로 구조적 문서를 만들 수 있는 방법을 제공해 주는 언어이다.\n' +
+    '</p>\n' +
+    '<span id="referenceTitle">참조</span>\n' +
+    '<ul class="description" style="font-size: 14px">\n' +
+    '    <li><a href="https://ko.wikipedia.org/wiki/%ED%95%98%EC%9D%B4%ED%8D%BC%ED%85%8D%EC%8A%A4%ED%8A%B8" target="_blank">\n' +
+    '        https://ko.wikipedia.org/wiki/%ED%95%98%EC%9D%B4%ED%8D%BC%ED%85%8D%EC%8A%A4%ED%8A%B8</a></li>\n' +
+    '    <li><a href="https://ko.wikipedia.org/wiki/%EB%A7%88%ED%81%AC%EC%97%85_%EC%96%B8%EC%96%B4">\n' +
+    '        https://ko.wikipedia.org/wiki/%EB%A7%88%ED%81%AC%EC%97%85_%EC%96%B8%EC%96%B4</a></li>\n' +
+    '    <li><a href="https://ko.wikipedia.org/wiki/HTML" target="_blank">https://ko.wikipedia.org/wiki/HTML</a></li>\n' +
+    '    <li><a href="https://brunch.co.kr/@coveryou/14" target="_blank">https://brunch.co.kr/@coveryou/14</a></li>\n' +
+    '</ul>'
---
+++ js/selfClosingTagHTML.js
@@ -0,0 +1,34 @@
+const selfClosingTagHTMLString = '<span class="subTitle">\n' +
+    '    \'셀프 클로징 태그(Self-Closing tags)\'?\n' +
+    '</span>\n' +
+    '<p class="description">\n' +
+    '    열자마자 닫아서 사용할 수 있는 태그. 원래 HTML 요소는 여는 태그와 닫는 태그가 필수적인데, 셀프 클로징 태그는 컨텐츠를 쓸 수 없도록 설계가 되었으며 태그 자체로만 사용하게 되어 있다. 그래서 void-elements라고 부른다.\n' +
+    '</p>\n' +
+    '<span class="subTitle">\n' +
+    '    왜 생겼을까\n' +
+    '</span>\n' +
+    '<p class="description">\n' +
+    '    컨텐츠가 없이 사용되는 태그, 즉 단일로 사용할 수 있는 태그임을 명시하기 위해 생겨났다.\n' +
+    '</p>\n' +
+    '<span class="subTitle">대표적인 셀프 클로징 태그들</span><br>\n' +
+    '    <div class="chipView">\n' +
+    '        <span>&lt;area /&gt;</span>\n' +
+    '        <span>&lt;base /&gt;</span>\n' +
+    '        <span>&lt;br /&gt;</span>\n' +
+    '        <span>&lt;col /&gt;</span>\n' +
+    '        <span>&lt;embed /&gt;</span>\n' +
+    '        <span>&lt;hr /&gt;</span>\n' +
+    '        <span>&lt;img /&gt;</span>\n' +
+    '        <span>&lt;input /&gt;</span>\n' +
+    '        <span>&lt;link /&gt;</span>\n' +
+    '        <span>&lt;meta /&gt;</span>\n' +
+    '        <span>&lt;param /&gt;</span>\n' +
+    '        <span>&lt;source /&gt;</span>\n' +
+    '        <span>&lt;track /&gt;</span>\n' +
+    '        <span>&lt;wbr /&gt;</span>\n' +
+    '    </div>\n' +
+    '<div style="margin-top: 40px"></div>\n' +
+    '<span id="referenceTitle">참조</span>\n' +
+    '<ul class="description" style="font-size: 14px">\n' +
+    '    <li><a href="http://xahlee.info/js/html5_non-closing_tag.html" target="_blank">http://xahlee.info/js/html5_non-closing_tag.html</a></li>\n' +
+    '</ul>'
