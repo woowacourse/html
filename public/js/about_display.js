ba61dbb8a979932496c29d8a2b1a9d6349114fff
hwangbo <taekwonod714@naver.com>
2020-03-23 17:56:01 +0900

    feat: 서서히 나타나는 효과 적용

--- public/js/about_display.js
+++ public/js/about_display.js
@@ -2,13 +2,10 @@ function show_text() {
     var image_display = document.getElementById('introducing-image');
     var text_display = document.getElementById('introducing-text');
 
-    if (image_display.style.display == "none") {
-        image_display.style.display = "block";
-        text_display.style.display = "none";
-    } else {
+    if (image_display.style.display == "block") {
         image_display.style.display = "none";
         text_display.style.display = "block";
-    }
+    } 
 }
 
 function go_mission() {
