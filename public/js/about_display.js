d872bc92ed43fd8016f7789b2bac541ffbb92a03
hwangbo <taekwonod714@naver.com>
2020-03-23 18:40:36 +0900

    fix: introduce 이미지 사라짐이 활성화되지 않음

--- public/js/about_display.js
+++ public/js/about_display.js
@@ -2,10 +2,13 @@ function show_text() {
     var image_display = document.getElementById('introducing-image');
     var text_display = document.getElementById('introducing-text');
 
-    if (image_display.style.display == "block") {
+    if (image_display.style.display == "none") {
+        image_display.style.display = "block";
+        text_display.style.display = "none";
+    } else {
         image_display.style.display = "none";
         text_display.style.display = "block";
-    } 
+    }
 }
 
 function go_mission() {
